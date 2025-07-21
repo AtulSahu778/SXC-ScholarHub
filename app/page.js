10ddbea16b1774775c790cbf6035223c3204f8d9
Atul Sahu <127933681+AtulSahu778@users.noreply.github.com>
2025-07-21 22:41:10 +0530

    update

--- app/page.js
+++ app/page.js
@@ -64,7 +64,7 @@ export default function App() {
         const data = await response.json()
         setResources(data)
         
-        // Update bookmarked resources set if user is logged in
+        // Update bookmarked resources set if user is logged inn
         if (user) {
           fetchDashboardData()
         }
