64fbf92f797b4483828845d60bd313fa6590e3df
Atul Sahu <127933681+AtulSahu778@users.noreply.github.com>
2025-08-11 23:04:50 +0530

    added icon

--- app/layout.js
+++ app/layout.js
@@ -12,6 +12,7 @@ export default function RootLayout({ children }) {
     <>
       <Head>
         <meta charSet="UTF-8" />
+        <link rel="apple-touch-icon" href="/favicon.ico"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="theme-color" content="#0f172a" />
         <meta name="color-scheme" content="dark" />
